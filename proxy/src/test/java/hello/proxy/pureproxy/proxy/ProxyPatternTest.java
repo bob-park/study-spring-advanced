7ce513279153a9841f1ec4d6c64f7226cb61be8f
phw11 <hwpark11@kakao.com>
2021-11-02 14:00:06 +0900

    feat : 프록시 패턴 - 예제 코드2

--- proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,19 @@ class ProxyPatternTest {
     RealSubject realSubject = new RealSubject();
 
     ProxyPatternClient client = new ProxyPatternClient(realSubject);
+
+    client.execute();
+    client.execute();
+    client.execute();
+  }
+
+  @Test
+  void cacheProxyTest() {
+    RealSubject realSubject = new RealSubject();
+
+    CacheProxy cacheProxy = new CacheProxy(realSubject);
+
+    ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
 
     client.execute();
     client.execute();
