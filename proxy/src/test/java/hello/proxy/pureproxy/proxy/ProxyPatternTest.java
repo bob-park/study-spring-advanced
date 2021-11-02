4176656a5ddb98e2f73ee5ab49cfe10c5ad67be1
phw11 <hwpark11@kakao.com>
2021-11-02 13:53:17 +0900

    feat : 프록시 패턴 - 예제 코드1

---
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,21 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class ProxyPatternTest {
+
+  @Test
+  void noProxyTest() {
+    RealSubject realSubject = new RealSubject();
+
+    ProxyPatternClient client = new ProxyPatternClient(realSubject);
+
+    client.execute();
+    client.execute();
+    client.execute();
+  }
+}
