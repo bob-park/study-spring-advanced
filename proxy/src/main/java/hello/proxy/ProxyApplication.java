67675575e1a4165735c0c59a4177fc046ebf7617
phw11 <hwpark11@kakao.com>
2021-11-02 13:35:53 +0900

    feat : 예제 프로젝트 만들기 v2

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,13 @@
 package hello.proxy;
 
 import hello.proxy.configure.AppV1Config;
+import hello.proxy.configure.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
+//@Import(AppV1Config.class)
+@Import(AppV2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
