8fd93d0bb30d53d43481d1be2c825b7a86ebfe10
Bob Park <hwpark@malgn.com>
2021-11-08 00:08:01 +0900

    feat : within

---
+++ aop/src/test/java/hello/aop/pointcut/WithinTest.java
@@ -0,0 +1,33 @@
+package hello.aop.pointcut;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hello.aop.member.MemberServiceImpl;
+import java.lang.reflect.Method;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+@Slf4j
+class WithinTest {
+
+    AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+
+    Method helloMethod;
+
+    @BeforeEach
+    public void init() throws NoSuchMethodException {
+        helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+    }
+
+    @Test
+    void withinExact() {
+        // execution 에서 타입부분 사용하는 것과 동일
+        // ! 정확한 타입이어야함
+        // ! 부모타입 안됨
+        pointcut.setExpression("within(hello.aop.member.MemberServiceImpl)");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+}
